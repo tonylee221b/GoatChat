adc674576a14e4c21112f8b3a7ff88faf231f583
mgyoo <mgyoo@autocrypt.io>
2026-08-03 19:21:36 +0900

    chat domain apply transaction

--- server/internal/chat/application/chat_room_service.go
+++ server/internal/chat/application/chat_room_service.go
@@ -7,35 +7,42 @@ import (
 
 	port "GoatChat/GoatChat/internal/chat/application/port"
 	"GoatChat/GoatChat/internal/chat/domain"
+	dbtx "GoatChat/GoatChat/internal/shared/db_tx"
 )
 
 type ChatService struct {
+	tx   dbtx.Tx
 	repo port.ChatRepository
 }
 
-func NewChatService(repo port.ChatRepository) *ChatService {
-	return &ChatService{repo}
+func NewChatService(tx dbtx.Tx, repo port.ChatRepository) *ChatService {
+	return &ChatService{tx, repo}
 }
 
-// TODO (mgyoo) : transaction 추가시 변경예정
 func (svc *ChatService) CreateChatroom(ctx context.Context,
 	rt domain.RoomType,
 	rn domain.RoomName,
 	d domain.RoomDescription,
 	oid domain.RoomOwnerId,
 ) (*domain.ChatRoom, error) {
-	cr, err := domain.NewChatroom(rt, rn, d, oid)
-	if err != nil {
-		slog.Info("failed to create chat room", "err", err.Error())
-		return nil, errors.New("failed to create chat room, error: " + err.Error())
-	}
-	slog.Debug("chatroom domain: ", slog.Any("Chatroom", *cr))
+	var cr *domain.ChatRoom
+	err := svc.tx.WithinTx(ctx, func(ctx context.Context) error {
+		newCr, err := domain.NewChatroom(rt, rn, d, oid)
+		if err != nil {
+			slog.Info("failed to create chat room", "err", err.Error())
+			return errors.New("failed to create chat room, error: " + err.Error())
+		}
+		slog.Debug("chatroom domain: ", slog.Any("Chatroom", *newCr))
 
-	err = svc.repo.SaveChatroom(ctx, *cr)
-	if err != nil {
-		slog.Info("failed to save chat room", "err", err.Error())
-		return nil, errors.New("failed to save chat room, error: " + err.Error())
-	}
+		err = svc.repo.SaveChatroom(ctx, *newCr)
+		if err != nil {
+			slog.Info("failed to save chat room", "err", err.Error())
+			return errors.New("failed to save chat room, error: " + err.Error())
+		}
+		cr = newCr
 
-	return cr, nil
+		return nil
+	})
+
+	return cr, err
 }
